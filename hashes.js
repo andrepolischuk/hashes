ef6208d1d0ff9a3c99b25044589fa01ef7f6b304
andrepolischuk <andre.polischuk@gmail.com>
2014-10-21 11:36:11 +0400

    Update API

--- hashes.js
+++ hashes.js
@@ -3,165 +3,148 @@
 
 !function(undefined) {
 
-  var Hashes = function(params) {
+  'use strict';
 
-    var glob = {};
+  /**
+   * Hash prefix
+   */
 
-    /**
-     * Default hash prefix
-     */
-    
-    glob.pref  = '#!';
-    
-    /**
-     * Default site index route
-     */
-    
-    glob.index = '#!/';
+  var pref = '#';
 
-    /**
-     * Previous route
-     */
-    
-    var pre  = null;
+  /**
+   * Index route
+   */
 
-    /**
-     * Current route
-     */
-    
-    var cur  = null;
-    
-    /**
-     * Current location.hash
-     */
-    
-    var hash = null;
+  var index = '/';
 
-    /**
-     * Msie 8- detection
-     */
-    
-    var msie = (/MSIE.\d+\./gi.test(navigator.userAgent) && parseInt(navigator.userAgent.replace(/.*MSIE.(\d+)\..*/gi, "$1")) < 8);
+  /**
+   * Previous route
+   */
 
-    /**
-     * Hash fix for msie 8-
-     */
-    
-    if (msie) {
-      var hashMsie = window.location.hash;
+  var previous;
+
+  /**
+   * Callback
+   */
+
+  var callback;
+
+  /**
+   * MSIE 8- detection
+   */
+
+  var msie = /MSIE.\d+\./gi.test(navigator.userAgent) &&
+    parseInt(navigator.userAgent.replace(/.*MSIE.(\d+)\..*/gi, "$1")) < 8;
+
+  /**
+   * Start hash length check
+   */
+
+  if (window.location.hash.length < 2) {
+    window.location.hash = index;
+  }
+
+  /**
+   * Hash expression
+   * @api private
+   */
+
+  var exp = function() {
+    return new RegExp('^' + pref + '(.+)$');
+  };
+
+  /**
+   * Hash change event callback
+   * @api private
+   */
+
+  var locationListener = function() {
+
+    var current = window.location.hash.replace(exp(), "$1");
+
+    if (previous === current) {
+      return false;
     }
 
-    /**
-     * Hash change event callback
-     * @api private
-     */
-    
-    var locationListener = function() {
+    previous = current;
 
-      hash = window.location.hash;
-
-      if (hash.length < 3) {
-        window.location.hash = glob.index;
-        return false;
-      } else {
-        cur = hash.replace(/^#!(.+)$/, "$1");
-      }
-
-      // set prevous url
-      if (pre !== cur) {
-        pre = cur;
-      }
-
-      // call global callback
-      if ('function' === typeof(glob.callback)) {
-        glob.callback(cur);
-      }
-
-    };
-
-    /**
-     * Internal redirect /index -> /#!/index
-     * @param {String} location
-     * @api public
-     */
-    
-    this.redirectInternal = function(location) {
-      if (location !== undefined) {
-        window.location.hash = glob.pref + location;
-      }
-    };
-
-    /**
-     * External redirect /index -> /index
-     * @param {String} location
-     * @api public
-     */
-    
-    this.redirectExternal = function(location) {
-      if (location !== undefined) {
-        window.location = location;
-      }
-    };
-
-    /**
-     * Set global params
-     */
-    
-    if (params !== undefined) {
-
-      if ('function' === typeof(params)) {
-        
-        // only callback
-        glob.callback = params;
-
-      } else {
-
-        // multiple parameters
-        for (var param in params) {
-          if (params.hasOwnProperty(param)) {
-            glob[param] = params[param];
-          }
-        }
-      }
-
-    }
-
-    /**
-     * Initialize hash change listener
-     */
-    
-    locationListener();
-
-    // when hash updated
-    if ('onhashchange' in window && msie === false) {
-
-      if ('addEventListener' in window) {
-        window.addEventListener('hashchange', locationListener, false);
-      } else if ('attachEvent' in window) {
-        window.attachEvent('onhashchange', locationListener);
-      }
-
-    } else {
-
-      (function locationListenerIe() {
-
-        if (hashMsie !== window.location.hash) {
-          locationListener();
-          hashMsie = window.location.hash;
-        }
-
-        setTimeout(locationListenerIe, 500);
-
-      })();
-
+    if (typeof callback === 'function') {
+      callback(current);
     }
 
   };
 
   /**
+   * Hashes
+   * @api public
+   */
+
+  function hashes(fn) {
+
+    if (typeof fn === 'function') {
+      callback = fn;
+      locationListener();
+    }
+
+  }
+
+  /**
+   * Internal redirect /index -> /#/index
+   * @param {String} route
+   * @api public
+   */
+
+  hashes.redirectInternal = function(route) {
+    if (!route) {
+      window.location.hash = pref + route;
+    }
+  };
+
+  /**
+   * External redirect /index -> /index
+   * @param {String} route
+   * @api public
+   */
+
+  hashes.redirectExternal = function(route) {
+    if (!route) {
+      window.location = route;
+    }
+  };
+
+  /**
+   * Hash change listener
+   */
+
+  if ('onhashchange' in window && !msie) {
+
+    if ('addEventListener' in window) {
+      window.addEventListener('hashchange', locationListener, false);
+    } else if ('attachEvent' in window) {
+      window.attachEvent('onhashchange', locationListener);
+    }
+
+  } else {
+
+    (function locationListenerIe(hash) {
+
+      if (hash !== window.location.hash) {
+        locationListener();
+        hash = window.location.hash;
+      }
+
+      setTimeout(function() {
+        locationListenerIe(hash);
+      }, 500);
+
+    })();
+
+  }
+
+  /**
    * Module exports
    */
-  
-  window.Hashes = Hashes;
+
+  window.hashes = hashes;
 
 }();
